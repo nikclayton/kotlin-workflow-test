13c6c8eece628f0db207d3cd033b8ae07fe382fe
Nik Clayton <nik@ngo.org.uk>
2023-07-27 19:17:20 +0200

    Update app/src/main/java/uk/org/ngo/workflowtest/FirstFragment.kt
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- app/src/main/java/uk/org/ngo/workflowtest/FirstFragment.kt
+++ app/src/main/java/uk/org/ngo/workflowtest/FirstFragment.kt
@@ -17,8 +17,7 @@ class FirstFragment : Fragment() {
 
     // This property is only valid between onCreateView and
     // onDestroyView.
-    private val binding get() =  _binding!!
-
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
